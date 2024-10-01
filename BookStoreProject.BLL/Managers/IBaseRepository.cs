403c447c65ea99aa44b620b28ca75feb839c560c
alishadad <alishadad86@yahoo.com>
2024-10-01 21:30:20 +0300

    add irepo

---
+++ BookStoreProject.BLL/Managers/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace BookStoreProject.BLL.Managers
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T GetByID(int id);
+        Task<T> GetByIDAsync(int id);
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T Find(Expression<Func<T, bool>> match);
+        Task<T> Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+
+    }
+}
